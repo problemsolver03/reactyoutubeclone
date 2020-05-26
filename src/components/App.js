af2eb540dc710fdea5148da1f222020a0ecbdcbe
problemsolver03 <lnov36630@gmail.com>
2020-05-26 12:25:36 +0530

    default videos

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,10 @@ class App extends React.Component{
     selectedVideo:null
   }
 
+  componentDidMount() {
+    this.getYoutubeVideos('Buildings')
+  }
+
 
   getYoutubeVideos = async (term) => { 
    
